ff7c21282b415766a77e996547847f761f149d35
Melody Daniel <melodyogonna@gmail.com>
2024-01-09 10:47:01 +0100

    Add vim dispatch

--- plugins/init.lua
+++ plugins/init.lua
@@ -17,11 +17,6 @@ return {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
   },
   {
     "amadeus/vim-mjml",
@@ -39,4 +34,8 @@ return {
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
+  {
+    "tpope/vim-dispatch",
+    event = "VeryLazy"
+  }
 }
